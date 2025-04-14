e5d3fefad472ddf51734f95ae7e0a9df0e206fa8
David <david.miguel.rocha@hotmail.com>
2025-04-14 20:34:47 +0200

    add light-compute implementation in spin, add env file to gitignore

--- light-compute/src/index.js
+++ light-compute/src/index.js
@@ -1,17 +1,59 @@
+import { AutoRouter } from "itty-router";
 
-// For AutoRouter documentation refer to https://itty.dev/itty-router/routers/autorouter
-import { AutoRouter } from 'itty-router';
+const router = AutoRouter();
 
-let router = AutoRouter();
-
-// Route ordering matters, the first route that matches will be used
-// Any route that does not return will be treated as a middleware
-// Any unmatched route will return a 404
 router
-    .get('/', () => new Response('Hello, Spin!'))
-    .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+  /*.get("/", () => new Response("Hello, Spin!"))
+  .get("/hello/:name", ({ params }) => new Response(`Hello, ${params.name}!`))*/
+  .post("/light-compute", async (request) => {
+    const start = performance.now();
 
-addEventListener('fetch', (event) => {
-    event.respondWith(router.fetch(event.request));
+    try {
+      const { matrixA, matrixB } = await request.json();
+
+      if (!Array.isArray(matrixA) || !Array.isArray(matrixB)) {
+        return new Response(
+          JSON.stringify({ message: "Missing or invalid matrix data" }),
+          { status: 400 }
+        );
+      }
+
+      if (matrixA[0].length !== matrixB.length) {
+        return new Response(
+          JSON.stringify({
+            message: "Incompatible matrices for multiplication",
+          }),
+          { status: 400 }
+        );
+      }
+
+      const resultMatrix = multiplyMatrices(matrixA, matrixB);
+      const executionDuration = performance.now() - start;
+
+      return new Response(JSON.stringify({ resultMatrix, executionDuration }), {
+        status: 200,
+      });
+    } catch (e) {
+      return new Response(JSON.stringify({ message: "Invalid JSON input" }), {
+        status: 400,
+      });
+    }
+  });
+
+addEventListener("fetch", (event) => {
+  event.respondWith(router.fetch(event.request));
 });
 
+function multiplyMatrices(A, B) {
+  const result = [];
+  for (let i = 0; i < A.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < B[0].length; j++) {
+      result[i][j] = 0;
+      for (let k = 0; k < A[0].length; k++) {
+        result[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return result;
+}
