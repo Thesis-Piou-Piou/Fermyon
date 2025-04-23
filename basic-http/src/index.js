a7bc22a0b7dd88f6b865710a699b16e08d35d72a
Isabela-Mitac <mitac@student.chalmers.se>
2025-04-23 15:05:21 +0200

    Update basic-http function

--- basic-http/src/index.js
+++ basic-http/src/index.js
@@ -12,25 +12,52 @@ router.get("/basic-http", async () => {
   // artificial delay for testing:
   //await new Promise((res) => setTimeout(res, 200)); // example delay
 
-  const message = "Function executed";
-  const end = performance.now();
+  const euCountries = [
+    { name: "Austria", code: "AT" },
+    { name: "Belgium", code: "BE" },
+    { name: "Bulgaria", code: "BG" },
+    { name: "Croatia", code: "HR" },
+    { name: "Cyprus", code: "CY" },
+    { name: "Czech Republic", code: "CZ" },
+    { name: "Denmark", code: "DK" },
+    { name: "Estonia", code: "EE" },
+    { name: "Finland", code: "FI" },
+    { name: "France", code: "FR" },
+    { name: "Germany", code: "DE" },
+    { name: "Greece", code: "GR" },
+    { name: "Hungary", code: "HU" },
+    { name: "Ireland", code: "IE" },
+    { name: "Italy", code: "IT" },
+    { name: "Latvia", code: "LV" },
+    { name: "Lithuania", code: "LT" },
+    { name: "Luxembourg", code: "LU" },
+    { name: "Malta", code: "MT" },
+    { name: "Netherlands", code: "NL" },
+    { name: "Poland", code: "PL" },
+    { name: "Portugal", code: "PT" },
+    { name: "Romania", code: "RO" },
+    { name: "Slovakia", code: "SK" },
+    { name: "Slovenia", code: "SI" },
+    { name: "Spain", code: "ES" },
+    { name: "Sweden", code: "SE" }
+  ];
 
-  const executionTime = end - start;
+  const execution = performance.now() - start;
 
   const body = JSON.stringify({
-    message,
-    execution: executionTime.toFixed(6), // in milliseconds
+    countries: euCountries,
+    execution: execution.toFixed(6) + " ms"
   });
 
   return new Response(body, {
     status: 200,
     headers: {
-      "Content-Type": "application/json",
-    },
+      "Content-Type": "application/json"
+    }
   });
 });
-//.get("/basic-http/hello/:name", ({ name }) => `Hello, ${name}!`);
 
+// Start handling incoming requests
 addEventListener("fetch", (event) => {
   event.respondWith(router.fetch(event.request));
 });
